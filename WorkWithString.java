87e3eea57ac9390d0eb1472ef799b92491f9ad97
Muqtar <tolafejay@gmail.com>
2021-11-05 05:00:14 +0100

    added new files

--- WorkWithString.java
+++ WorkWithString.java
@@ -1,5 +1,11 @@
 package neewPackage;
 
+import java.util.Locale;
+
+import static jdk.internal.org.jline.utils.AttributedStringBuilder.append;
+
+//import static jdk.internal.org.jline.utils.AttributedStringBuilder.append;
+
 public class WorkWithString {
     public static void main(String[] args) {
 
@@ -110,6 +116,134 @@ public class WorkWithString {
                 }
 
 
+                class fjjd{
+                    public static void main(String[] args) {
+
+                        String s1 = new String("happy birthday");
+
+                        for (int count = s1.length() - 1; count >= 0; count-- ){
+                            System.out.print(s1.charAt(count));
+                        }
+
+
+                        System.out.println(s1.length());
+
+                    }
+
+                }
+
+                class jdkk {
+                    public static void main(String[] args) {
+
+                        String[] s1 = {"amazing", "amani", "incredible", "increase"};
+
+                        for (String strings : s1) {
+                            if (strings.startsWith("inc")) {
+                                System.out.println(strings);
+                            }
+                        }
+                    }
+                }
+
+                    class jdk{
+                        public static void main(String[] args) {
+
+                            String letter =  "adnsmnncjsjhdvghbbdbsjd";
+
+                            System.out.println(letter.lastIndexOf('j'));
+                        }
+
+                    }
+
+                    class SubString{
+                        public static void main(String[] args) {
+
+                            String letters = "abcdefghijklmabcdefghijklm";
+
+                            System.out.println(letters.substring(20));
+                            System.out.println(letters.substring(3, 6));
+                        }
+
+                    }
+
+                    class UpperCase{
+                        public static void main(String[] args) {
+
+                            String s1 = "happy";
+                            String s2 = " BIRTHDAY";
+                            String s3 = "   spaces    ";
+                            System.out.println(s1.toUpperCase(Locale.ROOT));
+                            System.out.println(s2.toLowerCase(Locale.ROOT));
+                            System.out.println(s1.toUpperCase());
+                            System.out.println(s3.trim());
+                        }
+
+ }
+
+//
+//    class StringBuilderBB {
+//
+//
+//        // private StringBuilder append(Object objectRef)
+//
+//        public static void main(String[] args) {
+//
+//            Object objectRef = "hello";
+//            String string = "goodbye";
+//            char[] charArray = {'a', 'b', 'c', 'd', 'e', 'f'};
+//            boolean booleanValue = true;
+//            char characterValue = 'Z';
+//            int integerValue = 7;
+//            long longValue = 10000000000L;
+//            float floatValue = 2.5f;
+//            double doubleValue = 33.333;
+//
+//            StringBuilder lastBuffer = new StringBuilder("last buffer");
+//            StringBuilder buffer = new StringBuilder("hello, how are you");
+//
+//
+//            buffer.append(objectRef)
+//                    .append("%n");
+//            buffer.append(string)
+//                    .append("%n");
+//            buffer.append(charArray)
+//                    .append("%n");
+//            //  buffer.append(charArray, 0, 3)
+//            //        .append("%n");
+//            buffer.append(booleanValue)
+//                    .append("%n");
+//            buffer.append(characterValue)
+//                    .append("%n");
+//            buffer.append(integerValue)
+//                    .append("%n");
+//            buffer.append(longValue)
+//                    .append("%n");
+//            buffer.append(floatValue)
+//                    .append("%n");
+//            buffer.append(doubleValue)
+//                    .append("%n");
+//                    .append(lastBuffer);
+//
+//            System.out.println(buffer.toString());
+//        }
+//
+//    }
+
+    class kdk{
+        public static void main(String[] args) {
+
+
+
+            StringBuilder buffer = new StringBuilder("hello how are you ");
+
+            System.out.println(buffer.length());
+            System.out.println(buffer.capacity());
+            buffer.ensureCapacity(75);
+            System.out.println(buffer.capacity());
+
+
+        }
+    }
 
 
 
